45dcef9b18c732c4756bedfce314deac81e296f1
juliasoares17 <juliapereira@gmail.com>
2025-04-11 17:14:18 -0300

    Ajustes para que o módulo funcione corretamente

---
+++ out/metodos.d.ts
@@ -0,0 +1,13 @@
+declare class Somador {
+    calcular: (numero1: number, numero2: number) => number;
+}
+declare class Subtrador {
+    calcular: (numero1: number, numero2: number) => number;
+}
+declare class Divisor {
+    calcular: (numero1: number, numero2: number) => number;
+}
+declare class Multiplicador {
+    calcular: (numero1: number, numero2: number) => number;
+}
+export { Somador, Subtrador, Multiplicador, Divisor };
